e3ec3b12883d774d13bfc23cd8914c077456a826
Pavel Piatetskii <pavel.piatetskii@gmail.com>
2021-06-26 16:29:23 -0700

    Add User model to the api

--- api/models.py
+++ api/models.py
@@ -4,3 +4,7 @@ from __future__ import unicode_literals
 from django.db import models
 
 # Create your models here.
+class User(models.Model):
+  name = models.CharField(max_length=32, default="", unique=True)
+  password = models.CharField(max_length=256)
+  created_at = models.DateTimeField(auto_now_add=True)
