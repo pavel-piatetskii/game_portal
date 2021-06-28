cfc1ff26c6e35eabb292bb4c60d3b3f973027d35
Pavel Piatetskii <pavel.piatetskii@gmail.com>
2021-06-28 00:10:27 -0700

    Rename frontend app to frontend_portal

--- game_portal/urls.py
+++ game_portal/urls.py
@@ -19,5 +19,5 @@ from django.contrib import admin
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url('api/', include('api.urls')),
-    url('', include('frontend.urls')),
+    url('', include('frontend_portal.urls')),
 ]
