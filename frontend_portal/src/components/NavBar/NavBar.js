91b54a6e00ab0d9f6273282d33240c7762128cf9
Pavel Piatetskii <pavel.piatetskii@gmail.com>
2021-06-29 13:53:50 -0700

    Add font awesome icon and styling to NavBar and App

--- frontend_portal/src/components/NavBar/NavBar.js
+++ frontend_portal/src/components/NavBar/NavBar.js
@@ -1,13 +1,20 @@
 import React from 'react';
-
+import './NavBar.scss'
 export default function NavBar(props) {
 
   const {setContent} = props;
 
   return (<section className="navbar">
+    <div className="navbar__left">
       <p className="navbar__menu-item" onClick={() => setContent('homepage')}>Home</p>
       <p className="navbar__menu-item" onClick={() => setContent('games')}>Games</p>
       <p className="navbar__menu-item" onClick={() => setContent('highscores')}>Highscores</p>
+    </div>
+    <div className="navbar__center">
+      <span>GAME</span>
+      <i className="fa fa-shield navbar__center__icon"></i>
+      <span>PORTAL</span>
+    </div>
       <p className="navbar__menu-item" onClick={() => setContent('signup')}>Sign Up</p>
 
     </section>)
