2a8b7fc3500e591e377584e0e157030441cb9f49
Pavel Piatetskii <pavel.piatetskii@gmail.com>
2021-06-29 09:49:00 -0700

    Start creating general App logic

--- frontend_portal/src/components/App.js
+++ frontend_portal/src/components/App.js
@@ -1,8 +1,27 @@
 //import logo from './logo.svg';
 import './App.scss';
-import React from 'react';
-
+import React, {useState, useEffect} from 'react';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
 function App() {
+
+  const [content, setContent] = useState('homepage');
+  //const [showImageViewer, setShowImageViewer] = useState(false);
+
+  
+  // State controlling ghreetings component (now disabled for debugging)
+  // const [showGreetings, setShowGreetings] = useState(true);
+  const [contentComponent, setContentComponent] = useState('');  
+  
+  
+  useEffect(() => {
+    const contentComponents = {
+      homepage: <Homepage setContent={setContent}/>,
+      // games: <Projects projects={projects} showImageViewer={showImageViewer} setShowImageViewer={setShowImageViewer}/>,
+      // highscores: <About showImageViewer={showImageViewer} setShowImageViewer={setShowImageViewer}/>,
+    };
+    setContentComponent(contentComponents[content]);
+  }, [content]);
+
   return (
     <div className="App">
       <header className="App-header">
