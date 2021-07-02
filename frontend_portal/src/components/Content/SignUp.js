08b7bb795f53fc0f812b810e615c72f5cfd417fe
Pavel Piatetskii <pavel.piatetskii@gmail.com>
2021-07-01 20:43:23 -0700

    Add SignUp form

--- frontend_portal/src/components/Content/SignUp.js
+++ frontend_portal/src/components/Content/SignUp.js
@@ -1,7 +1,31 @@
 import React from 'react';
+import './SignUp.scss'
 
 export default function SignUp(props) {
   return (
-    <section>SignUp</section>
+    <section className="signup">
+      <form className="signup__form">
+        
+        <h2 className="signup__form__header">SIGN UP</h2>
+
+        <div className="signup__form__wrapper">
+          <label className="signup__form__label" htmlFor="signup-login">ENTER LOGIN</label>
+          <input className="signup__form__input" id="signup-login" name="login"></input>
+        </div>
+
+        <div className="signup__form__wrapper">
+          <label className="signup__form__label" htmlFor="signup-password">ENTER PASSWORD</label>
+          <input className="signup__form__input" id="signup-password" name="password"></input>
+        </div>
+
+        <div className="signup__form__wrapper">
+          <label className="signup__form__label" htmlFor="signup-password-repeat">REPEAT PASSWORD</label>
+          <input className="signup__form__input" id="signup-login-repeat" name="password-repeat"></input>
+        </div>
+
+        <button className="signup__form__button">SUBMIT</button>
+
+      </form>
+    </section>
   );
 };
