2fce3639b2bc38f2acc57bee724bc50d30b2d620
Pavel Piatetskii <pavel.piatetskii@gmail.com>
2021-07-04 18:44:39 -0700

    Add states for SignUp input fields

--- frontend_portal/src/components/Content/SignUp.js
+++ frontend_portal/src/components/Content/SignUp.js
@@ -1,7 +1,20 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './SignUp.scss'
 
 export default function SignUp(props) {
+
+  const [login, setLogin] = useState('');
+  const [password, setPassword] = useState('');
+  const [passwordRepeat, setPasswordRepeat] = useState('');
+  
+  const clickSubmit = function(e) {
+    e.preventDefault();
+    console.log(login);
+    console.log(password);
+    console.log(passwordRepeat);
+  }
+
+
   return (
     <section className="signup">
       <form className="signup__form">
@@ -10,20 +23,32 @@ export default function SignUp(props) {
 
         <div className="signup__form__wrapper">
           <label className="signup__form__label" htmlFor="signup-login">ENTER LOGIN</label>
-          <input className="signup__form__input" id="signup-login" name="login"></input>
+          <input 
+          className="signup__form__input" 
+          id="signup-login" 
+          name="login"
+          onChange={(e) => setLogin(e.target.value)}></input>
         </div>
 
         <div className="signup__form__wrapper">
           <label className="signup__form__label" htmlFor="signup-password">ENTER PASSWORD</label>
-          <input className="signup__form__input" id="signup-password" name="password"></input>
+          <input 
+          className="signup__form__input" 
+          id="signup-password" 
+          name="password"
+          onChange={(e) => setPassword(e.target.value)}></input>
         </div>
 
         <div className="signup__form__wrapper">
           <label className="signup__form__label" htmlFor="signup-password-repeat">REPEAT PASSWORD</label>
-          <input className="signup__form__input" id="signup-login-repeat" name="password-repeat"></input>
+          <input 
+          className="signup__form__input" 
+          id="signup-login-repeat" 
+          name="password-repeat"
+          onChange={(e) => setPasswordRepeat(e.target.value)}></input>
         </div>
 
-        <button className="signup__form__button">SUBMIT</button>
+        <button onClick={(e) => clickSubmit(e)} className="signup__form__button">SUBMIT</button>
 
       </form>
     </section>
