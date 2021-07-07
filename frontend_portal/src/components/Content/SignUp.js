5a12b8341fea83da53eb65bacc0c4dfcb2540b1d
Pavel Piatetskii <pavel.piatetskii@gmail.com>
2021-07-07 15:39:51 -0700

    Refactared input fields HTML

--- frontend_portal/src/components/Content/SignUp.js
+++ frontend_portal/src/components/Content/SignUp.js
@@ -3,11 +3,29 @@ import './SignUp.scss'
 
 export default function SignUp(props) {
 
-  const [login, setLogin] = useState('');
-  const [password, setPassword] = useState('');
-  const [passwordRepeat, setPasswordRepeat] = useState('');
 
-  const validateInput = function() {
+  const signUpForm = [
+    {
+      name: 'login',
+      label: 'ENTER LOGIN',
+      emptyMessage: 'Please enter login',
+      wrongMessage: 'Login should be 3-32 characters long',
+    },
+    {
+      name: 'password',
+      label: 'ENTER PASSWORD',
+      emptyMessage: 'Please enter password',
+      wrongMessage: 'password should be at least 8 characteres long',
+    }, 
+    {
+      name: 'password-repeat',
+      label: 'REPEAT PASSWORD',
+      emptyMessage: 'Please repeat password',
+      wrongMessage: 'Passwords do not match',
+    }, 
+  ];
+
+  const validateInput = function () {
 
     // Login
     const loginForm = document.getElementById('signup-login');
@@ -25,86 +43,108 @@ export default function SignUp(props) {
     if (!login) {
       loginForm.classList.add('input-error');
       exclaimLogin.classList.add('show');
-      popupLogin.innerText= 'Please enter login';
+      popupLogin.innerText = 'Please enter login';
       //popupLogin.classList.add('show');
     }
 
     if (!password) {
       passwordForm.classList.add('input-error');
       exclaimPassword.classList.add('show');
-      popupPassword.innerText= 'Please, enter password';
+      popupPassword.innerText = 'Please, enter password';
       //popupPassword.classList.add('show');
     }
 
     if (!passwordRepeat) {
       passwordRepeatForm.classList.add('input-error');
       exclaimRepeatPassword.classList.add('show');
-      popupRepeatPassword.innerText= 'Please, repeat password';
+      popupRepeatPassword.innerText = 'Please, repeat password';
       //popupRepeatPassword.classList.add('show');
     }
 
   };
-  
-  const clickSubmit = function(e) {
-    //console.log(e);
+
+  const clickSubmit = function (e) {
     e.preventDefault();
-    validateInput();
-    //console.log(login);
-    //console.log(password);
-    //console.log(passwordRepeat);
+    //signUpForm.map(field)
+    //validateInput();
   };
 
 
   return (
     <section className="signup">
       <form className="signup__form" onSubmit={(e) => clickSubmit(e)} >
-        
         <h2 className="signup__form__header">SIGN UP</h2>
 
-        <div className="signup__form__wrapper">
-          <label className="signup__form__label" htmlFor="signup-login">ENTER LOGIN</label>
-          <i id="exclaim-login" className="fa fa-exclamation-circle signup__form__exclaim">
-            <span id="popup-login" className="signup__form__popup"></span>
+        {signUpForm.map(field => 
+          <div className="signup__form__wrapper" key={field.name}>
+            <label className="signup__form__label" htmlFor={`signup-${field.name}`}>{field.label}</label>
+            <i id={`exclaim-${field.name}`} className="fa fa-exclamation-circle signup__form__exclaim">
+              <span id={`popup-${field.name}`} className="signup__form__popup"></span>
 
-          </i>
-          <input 
-          className="signup__form__input" 
-          id="signup-login" 
-          name="login"
-          onChange={(e) => setLogin(e.target.value)}
-          onFocus={(e) => e.target.classList.remove('input-error')}></input>
-        </div>
-
-        <div className="signup__form__wrapper">
-          <label className="signup__form__label" htmlFor="signup-password">ENTER PASSWORD</label>
-          <i id="exclaim-password" className="fa fa-exclamation-circle signup__form__exclaim">
-            <span id="popup-password" className="signup__form__popup"></span>
-          </i>
-          
-          <input 
-          className="signup__form__input" 
-          id="signup-password" 
-          name="password"
-          type="password"
-          onChange={(e) => setPassword(e.target.value)}></input>
-        </div>
-
-        <div className="signup__form__wrapper">
-          <label className="signup__form__label" htmlFor="signup-password-repeat">REPEAT PASSWORD</label>
-          <i id="exclaim-password-repeat" className="fa fa-exclamation-circle signup__form__exclaim">
-            <span id="popup-password-repeat" className="signup__form__popup"></span>
-          </i>
-          <input 
-          className="signup__form__input" 
-          id="signup-password-repeat" 
-          name="password-repeat"
-          type="password"
-          onChange={(e) => setPasswordRepeat(e.target.value)}></input>
-        </div>
-
+            </i>
+            <input
+              className="signup__form__input"
+              id={`signup-${field.name}`}
+              name={`${field.name}`}
+              onChange={(e) => setLogin(e.target.value)}
+              onFocus={(e) => e.target.classList.remove('input-error')}></input>
+          </div>
+        )}
         <button className="signup__form__button">SUBMIT</button>
-
       </form>
     </section>
+
+
+
+    // <section className="signup">
+    //   <form className="signup__form" onSubmit={(e) => clickSubmit(e)} >
+
+    //     <h2 className="signup__form__header">SIGN UP</h2>
+
+    //     <div className="signup__form__wrapper">
+    //       <label className="signup__form__label" htmlFor="signup-login">ENTER LOGIN</label>
+    //       <i id="exclaim-login" className="fa fa-exclamation-circle signup__form__exclaim">
+    //         <span id="popup-login" className="signup__form__popup"></span>
+
+    //       </i>
+    //       <input 
+    //       className="signup__form__input" 
+    //       id="signup-login" 
+    //       name="login"
+    //       onChange={(e) => setLogin(e.target.value)}
+    //       onFocus={(e) => e.target.classList.remove('input-error')}></input>
+    //     </div>
+
+    //     <div className="signup__form__wrapper">
+    //       <label className="signup__form__label" htmlFor="signup-password">ENTER PASSWORD</label>
+    //       <i id="exclaim-password" className="fa fa-exclamation-circle signup__form__exclaim">
+    //         <span id="popup-password" className="signup__form__popup"></span>
+    //       </i>
+
+    //       <input 
+    //       className="signup__form__input" 
+    //       id="signup-password" 
+    //       name="password"
+    //       type="password"
+    //       onChange={(e) => setPassword(e.target.value)}></input>
+    //     </div>
+
+    //     <div className="signup__form__wrapper">
+    //       <label className="signup__form__label" htmlFor="signup-password-repeat">REPEAT PASSWORD</label>
+    //       <i id="exclaim-password-repeat" className="fa fa-exclamation-circle signup__form__exclaim">
+    //         <span id="popup-password-repeat" className="signup__form__popup"></span>
+    //       </i>
+    //       <input 
+    //       className="signup__form__input" 
+    //       id="signup-password-repeat" 
+    //       name="password-repeat"
+    //       type="password"
+    //       onChange={(e) => setPasswordRepeat(e.target.value)}></input>
+    //     </div>
+
+    //     <button className="signup__form__button">SUBMIT</button>
+
+    //   </form>
+    // </section>
   );
 };
