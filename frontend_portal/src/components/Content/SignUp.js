7eca835d66536b41983451b333311d179b539926
Pavel Piatetskii <pavel.piatetskii@gmail.com>
2021-07-08 23:18:40 -0700

    Clean up and comment SignUp component

--- frontend_portal/src/components/Content/SignUp.js
+++ frontend_portal/src/components/Content/SignUp.js
@@ -3,7 +3,7 @@ import './SignUp.scss'
 
 export default function SignUp(props) {
 
-
+  // Data for input fields, thier validation and error messaging
   const signUpForm = [
     {
       name: 'login',
@@ -37,53 +37,18 @@ export default function SignUp(props) {
     }, 
   ];
 
-  // const validateInput = function () {
-
-  //   // Login
-  //   const loginForm = document.getElementById('signup-login');
-  //   const exclaimLogin = document.getElementById('exclaim-login');
-  //   const popupLogin = document.getElementById('popup-login');
-
-  //   const passwordForm = document.getElementById('signup-password');
-  //   const exclaimPassword = document.getElementById('exclaim-password');
-  //   const popupPassword = document.getElementById('popup-password');
-
-  //   const passwordRepeatForm = document.getElementById('signup-password-repeat');
-  //   const exclaimRepeatPassword = document.getElementById('exclaim-password-repeat');
-  //   const popupRepeatPassword = document.getElementById('popup-password-repeat');
-
-  //   if (!login) {
-  //     loginForm.classList.add('input-error');
-  //     exclaimLogin.classList.add('show');
-  //     popupLogin.innerText = 'Please enter login';
-  //     //popupLogin.classList.add('show');
-  //   }
-
-  //   if (!password) {
-  //     passwordForm.classList.add('input-error');
-  //     exclaimPassword.classList.add('show');
-  //     popupPassword.innerText = 'Please, enter password';
-  //     //popupPassword.classList.add('show');
-  //   }
-
-  //   if (!passwordRepeat) {
-  //     passwordRepeatForm.classList.add('input-error');
-  //     exclaimRepeatPassword.classList.add('show');
-  //     popupRepeatPassword.innerText = 'Please, repeat password';
-  //     //popupRepeatPassword.classList.add('show');
-  //   }
-  // };
-
+  /** Check that each input field contains 
+   *  text and this text fits requirements */
   const validateInput = function () {
     for (const field of signUpForm) {
 
-      const { name, emptyMessage, wrongMessage } = field;
+      const { name, emptyMessage, wrongMessage, checkCorrect } = field;
 
       const input = document.getElementById(`signup-${name}`);
       const exclaim = document.getElementById(`exclaim-${name}`);
       const popup = document.getElementById(`popup-${name}`);
       
-      if (!input.value || !field.checkCorrect(input.value)) {
+      if (!input.value || !checkCorrect(input.value)) {
         input.classList.add('input-error');
         exclaim.classList.add('show');
         popup.innerText = (!input.value) ? emptyMessage : wrongMessage;
@@ -91,13 +56,18 @@ export default function SignUp(props) {
     }
   }
 
+  // Clear error style, exclamation sign and message 
+  // (for input field onFocus attribute)
   const clearError = function(id) {
     document.getElementById(id).classList.remove('input-error');
     document.getElementById(`exclaim-${id.replace(/signup-/, '')}`).classList.remove('show');
   }
 
+  // Actions on sign-up form submit
   const clickSubmit = function (e) {
     e.preventDefault();
+
+    // Perform input fields validation
     validateInput();
   };
 
@@ -120,7 +90,6 @@ export default function SignUp(props) {
               name={`${field.name}`}
               autoComplete='off'
               type={field.type}
-              //onChange={(e) => setLogin(e.target.value)}
               onFocus={(e) => clearError(e.target.id)}></input>
           </div>
         )}
@@ -128,57 +97,5 @@ export default function SignUp(props) {
       </form>
     </section>
 
-
-
-    // <section className="signup">
-    //   <form className="signup__form" onSubmit={(e) => clickSubmit(e)} >
-
-    //     <h2 className="signup__form__header">SIGN UP</h2>
-
-    //     <div className="signup__form__wrapper">
-    //       <label className="signup__form__label" htmlFor="signup-login">ENTER LOGIN</label>
-    //       <i id="exclaim-login" className="fa fa-exclamation-circle signup__form__exclaim">
-    //         <span id="popup-login" className="signup__form__popup"></span>
-
-    //       </i>
-    //       <input 
-    //       className="signup__form__input" 
-    //       id="signup-login" 
-    //       name="login"
-    //       onChange={(e) => setLogin(e.target.value)}
-    //       onFocus={(e) => e.target.classList.remove('input-error')}></input>
-    //     </div>
-
-    //     <div className="signup__form__wrapper">
-    //       <label className="signup__form__label" htmlFor="signup-password">ENTER PASSWORD</label>
-    //       <i id="exclaim-password" className="fa fa-exclamation-circle signup__form__exclaim">
-    //         <span id="popup-password" className="signup__form__popup"></span>
-    //       </i>
-
-    //       <input 
-    //       className="signup__form__input" 
-    //       id="signup-password" 
-    //       name="password"
-    //       type="password"
-    //       onChange={(e) => setPassword(e.target.value)}></input>
-    //     </div>
-
-    //     <div className="signup__form__wrapper">
-    //       <label className="signup__form__label" htmlFor="signup-password-repeat">REPEAT PASSWORD</label>
-    //       <i id="exclaim-password-repeat" className="fa fa-exclamation-circle signup__form__exclaim">
-    //         <span id="popup-password-repeat" className="signup__form__popup"></span>
-    //       </i>
-    //       <input 
-    //       className="signup__form__input" 
-    //       id="signup-password-repeat" 
-    //       name="password-repeat"
-    //       type="password"
-    //       onChange={(e) => setPasswordRepeat(e.target.value)}></input>
-    //     </div>
-
-    //     <button className="signup__form__button">SUBMIT</button>
-
-    //   </form>
-    // </section>
   );
 };
