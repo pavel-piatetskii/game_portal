f27640ecf906295526d3d6e5aea5ce542e2e7a9d
Pavel Piatetskii <pavel.piatetskii@gmail.com>
2021-07-20 13:06:11 -0700

    Commit before merge

--- frontend_portal/src/components/App.js
+++ frontend_portal/src/components/App.js
@@ -16,7 +16,7 @@ function App() {
   //const [showImageViewer, setShowImageViewer] = useState(false);
 
   // State controlling ghreetings component (now disabled for debugging)
-  const [showGreeting, setShowGreeting] = useState(false);
+  // const [showGreeting, setShowGreeting] = useState(false);
 
   return (
     <div className="App">
