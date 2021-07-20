20ac41cd95f7381645acc262e8b76b1cfb7d8f58
Pavel Piatetskii <pavel.piatetskii@gmail.com>
2021-07-20 13:40:33 -0700

    Restore some files

--- frontend_portal/src/components/App.js
+++ frontend_portal/src/components/App.js
@@ -1,43 +1,47 @@
 //import logo from './logo.svg';
 import './App.scss';
+import Greeting from './Greeting';
 import Homepage from './Content/Homepage'
 import Games from './Content/Games'
 import Highscores from './Content/Highscores'
 import SignUp from './Content/SignUp'
 import NavBar from './NavBar/NavBar'
 import Footer from './Footer';
-import React, {useState, useEffect} from 'react';
+import React, { useState, useEffect } from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
 
 function App() {
 
   //const [showImageViewer, setShowImageViewer] = useState(false);
-  
+
   // State controlling ghreetings component (now disabled for debugging)
   // const [showGreeting, setShowGreeting] = useState(false);
 
   return (
-    <Router>
-      <div className="App">
-        <NavBar />
-        <Switch>
-          <Route exact path="/">
-            <Homepage />
-          </Route>
-          <Route path="/games">
-            <Games />
-          </Route>
-          <Route path="/highscores">
-            <Highscores />
-          </Route>
-          <Route path="/signup">
-            <SignUp />
-          </Route>
-        </Switch>
-        <Footer />
-      </div>
-    </Router>
+    <div className="App">
+      {showGreeting && <Greeting setShowGreeting={setShowGreeting}/>}
+      {!showGreeting && <Router>
+        <div className="App">
+          <NavBar />
+          <Switch>
+            <Route exact path="/">
+              <Homepage />
+            </Route>
+            <Route path="/games">
+              <Games />
+            </Route>
+            <Route path="/highscores">
+              <Highscores />
+            </Route>
+            <Route path="/signup">
+              <SignUp />
+            </Route>
+          </Switch>
+          <Footer />
+        </div>
+      </Router>}
+    </div>
   );
 }
 
